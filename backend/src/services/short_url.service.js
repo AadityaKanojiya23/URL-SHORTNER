4419faa926fcfed8f75049c07cc961640d933d22
AADITYA KANOJIYA <adityastudent750@gmail.com>
2026-08-22 23:40:36 +0530

    Refactor createShortUrlWithoutService function

--- backend/src/services/short_url.service.js
+++ backend/src/services/short_url.service.js
@@ -1,7 +1,7 @@
 import { generateNanoid } from "../utils/helper.js";
 import urlSchema from "../models/short_url.model.js";
 import { saveUrlService } from "../dao/short_url.js";
- 
+
 
 export const createShortUrlWithoutService = async (url) => {
    const shortUrl = await generateNanoid(7);
